429e82fecac63fc5a82f9fa7708466a3ce3bc9e5
gezichtshaar <tedteddet@gmail.com>
2013-12-06 11:54:57 +0100

    hashkeynavigation shitzels

--- public/js/global.js
+++ public/js/global.js
@@ -1,13 +1,27 @@
-initPage = function () {
-	var accessPages = ['home', 'gedichten'],
-		page = document.location.search.substr(1),
+resetPageElements = function () {
+	var elements = document.getElementsByClassName("page-element"),
+		elementCount = elements.length;
+
+	while (elementCount--) {
+		elements[elementCount].style.visibility = "hidden";
+	}
+}
+
+hashKeyNavigation = function () {
+	var page = document.location.hash.substr(1),
 		element = document.getElementById(page);
 
-	if (element !== null && accessPages.indexOf(page) >=0) {
+	resetPageElements();
+	if (element !== null && element.className === "page-element") {
 		element.style.visibility = "visible";
-	}else if (accessPages.length > 0) {
-		document.getElementById(accessPages[0]).style.visibility = "visible";
+	}else if ((element = document.getElementsByClassName("page-element")).length > 0) {
+		element[0].style.visibility = "visible";
 	}
+}
+
+initPage = function () {
+	hashKeyNavigation();
+	window.onhashchange = hashKeyNavigation;
 }
 
 initRequsts.push(initPage);
